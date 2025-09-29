93842a1f104f4ae2a350ba16a0065bd1890d46b3
Reem-A-Hikal <reemhikal19@gmail.com>
2025-09-30 01:53:50 +0300

    feat(overview): add dashboard layout and static recent activity section

--- src/app/components/overview/overview.component.ts
+++ src/app/components/overview/overview.component.ts
@@ -1,12 +1,15 @@
+import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-overview',
   standalone: true,
+  imports: [DatePipe],
   templateUrl: './overview.component.html',
   styleUrls: ['./overview.component.css'],
 })
 export class OverviewComponent implements OnInit {
+  date: Date = new Date();
   constructor() {}
 
   ngOnInit() {}
